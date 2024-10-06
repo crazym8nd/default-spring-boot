bd859934beb4c66d660b79e73f7172ad7bde43cb
crazym8nd <2vitalyd@gmail.com>
2024-10-06 16:12:40 +0300

    removed Data annotation from teacher

--- src/main/java/com/spring/crud/dao/entity/Teacher.java
+++ src/main/java/com/spring/crud/dao/entity/Teacher.java
@@ -8,14 +8,20 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
 import org.springframework.data.domain.Persistable;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
 @Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
@@ -40,5 +46,34 @@ public class Teacher implements Persistable<Long> {
     @Override
     public boolean isNew() {
         return (id == null);
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o)
+                .getHibernateLazyInitializer()
+                .getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this)
+                .getHibernateLazyInitializer()
+                .getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) {
+            return false;
+        }
+        Teacher teacher = (Teacher) o;
+        return getId() != null && Objects.equals(getId(), teacher.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this)
+                .getHibernateLazyInitializer()
+                .getPersistentClass()
+                .hashCode() : getClass().hashCode();
     }
 }
